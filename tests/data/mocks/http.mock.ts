6a3f888a16fc65b51c07187842c30c532f5f7468
Werlley Gonçalves <werlley.registros@hotmail.com>
2024-07-05 01:24:48 -0300

    mock(data): create http mock

---
+++ tests/data/mocks/http.mock.ts
@@ -0,0 +1,33 @@
+import {
+  HttpRequest,
+  HttpResponse,
+  HttpStatusCode,
+  HttpClient,
+} from "@/data/protocols/http";
+
+import { faker } from "@faker-js/faker";
+
+export const mockHttpRequest = (): HttpRequest => ({
+  url: faker.internet.url(),
+  method: "get",
+  body: faker.database.collation(),
+  headers: faker.database.collation(),
+});
+
+export class HttpClientSpy<R = any> implements HttpClient<R> {
+  url?: string;
+  method?: string;
+  body?: any;
+  headers?: any;
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok,
+  };
+
+  async request(data: HttpRequest): Promise<HttpResponse<R>> {
+    this.url = data.url;
+    this.method = data.method;
+    this.body = data.body;
+    this.headers = data.headers;
+    return this.response;
+  }
+}
