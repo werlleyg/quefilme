b6e71d5ea7011ec8f0657a585bbc3ddab96c3c1d
Werlley Gonçalves <werlley.registros@hotmail.com>
2024-07-30 23:06:46 -0300

    feat(domainEntities): create ListMoviesEntity

---
+++ src/domain/entities/listMovies.entity.ts
@@ -0,0 +1,38 @@
+import { MovieEntity, MovieEntityType } from "./movie.entity";
+
+export type ListMoviesEntityType = {
+  movies: MovieEntity[];
+};
+
+export class ListMoviesEntity {
+  constructor(public props: ListMoviesEntityType) {}
+
+  get movies(): MovieEntity[] {
+    return this.props.movies;
+  }
+
+  get toJSON(): { movies: MovieEntityType[] } {
+    return {
+      movies: this.props.movies.map((movie) => ({
+        image: movie.image,
+        imdbID: movie.imdbID,
+        title: movie.image,
+        type: movie.type,
+        actors: movie.actors,
+        description: movie.description,
+        genre: movie.genre,
+        runtime: movie.runtime,
+      })),
+    };
+  }
+
+  public addMovie(movie: MovieEntity): void {
+    this.props.movies = [...this.props.movies, movie];
+  }
+
+  public removeMovie(imdbID: string): void {
+    this.props.movies = this.props.movies.filter(
+      (movie) => movie.imdbID !== imdbID,
+    );
+  }
+}
