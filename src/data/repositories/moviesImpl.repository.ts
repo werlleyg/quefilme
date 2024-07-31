6293c757b65db1de7213cb15a11c4b252112a248
Werlley Gonçalves <werlley.registros@hotmail.com>
2024-07-30 23:10:32 -0300

    refactor(dataRepositories): change MoviesRepositoryImpl methods

--- src/data/repositories/moviesImpl.repository.ts
+++ src/data/repositories/moviesImpl.repository.ts
@@ -1,9 +1,17 @@
 import { Result } from "./../../../node_modules/@next/eslint-plugin-next/node_modules/glob/dist/commonjs/glob.d";
-import { MovieEntity } from "@/domain/entities";
+import {
+  ListMoviesEntity,
+  MovieEntity,
+  MovieEntityType,
+} from "@/domain/entities";
 import { MoviesRepository } from "@/domain/repositories";
 import { HttpClient, HttpStatusCode } from "../protocols/http";
 import { Config } from "@/shared";
-import { AccessDeniedError, UnexpectedError } from "@/domain/errors";
+import {
+  AccessDeniedError,
+  NotFoundError,
+  UnexpectedError,
+} from "@/domain/errors";
 import { MovieEntityHelper } from "@/helpers";
 
 export class MoviesRepositoryImpl implements MoviesRepository {
@@ -13,31 +21,42 @@ export class MoviesRepositoryImpl implements MoviesRepository {
   private readonly accessKey: string = Config.accessKey;
 
   async getMovie(imdbID: string): Promise<MovieEntity> {
-    const httpResponse = await this.httpClient.request({
+    let httpResponse = await this.httpClient.request({
       url: `${this.url}?i=${imdbID}&apikey=${this.accessKey}`,
       method: "get",
     });
 
     const result = httpResponse.body;
+
+    if (result?.Response === "False")
+      httpResponse = { ...httpResponse, statusCode: HttpStatusCode.notFound };
+
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        return MovieEntityHelper.fromJson(result);
+        return new MovieEntity(MovieEntityHelper.fromJson(result));
       case HttpStatusCode.forbidden:
         throw new AccessDeniedError();
+      case HttpStatusCode.notFound:
+        throw new NotFoundError();
       default:
         throw new UnexpectedError();
     }
   }
 
-  async getMovieList(title: string): Promise<MovieEntity[]> {
+  async getMovieList(title: string): Promise<ListMoviesEntity> {
     const httpResponse = await this.httpClient.request({
       url: `${this.url}?s=${title}&apikey=${this.accessKey}`,
       method: "get",
     });
     const result = httpResponse.body;
+
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        return MovieEntityHelper.fromJsonList(result?.Search) ?? [];
+        const movies = (
+          MovieEntityHelper.fromJsonList(result?.Search) ?? []
+        ).map((movie) => new MovieEntity(movie));
+        return new ListMoviesEntity({ movies });
+
       case HttpStatusCode.forbidden:
         throw new AccessDeniedError();
       default:
