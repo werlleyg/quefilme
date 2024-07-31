db5a5ff2534ee1b3618973824c979384d54ec592
Werlley Gonçalves <werlley.registros@hotmail.com>
2024-07-30 23:06:24 -0300

    feat(domainEntities): create MovieEntity

--- src/domain/entities/movie.entity.ts
+++ src/domain/entities/movie.entity.ts
@@ -8,3 +8,52 @@ export type MovieEntityType = {
   actors?: string;
   description?: string;
 };
+
+export class MovieEntity {
+  constructor(public props: MovieEntityType) {}
+
+  get title(): string {
+    return this.props.title;
+  }
+
+  get imdbID(): string {
+    return this.props.imdbID;
+  }
+
+  get type(): string {
+    return this.props.type;
+  }
+
+  get image(): string {
+    return this.props.image;
+  }
+
+  get runtime(): string {
+    return this.props.runtime ?? "";
+  }
+
+  get genre(): string {
+    return this.props.genre ?? "";
+  }
+
+  get actors(): string {
+    return this.props.actors ?? "";
+  }
+
+  get description(): string {
+    return this.props.description ?? "";
+  }
+
+  get toJSON(): MovieEntityType {
+    return {
+      image: this.props.image,
+      imdbID: this.props.imdbID,
+      title: this.props.image,
+      type: this.props.type,
+      actors: this.props.actors,
+      description: this.props.description,
+      genre: this.props.genre,
+      runtime: this.props.runtime,
+    };
+  }
+}
