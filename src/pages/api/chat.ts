9b9ff3c8732dc8f8e766c06c8be74e5f85fbfa91
Werlley Gonçalves <werlley.registros@hotmail.com>
2024-08-20 23:31:45 -0300

    feat(api): create chat endpoint

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,25 @@
+import { ChatGPTAiClient } from "@/infrastructure/ai/chatGPTAiClient.infra";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { query, method } = req;
+
+  if (req.method === "OPTIONS") {
+    res.status(204).end();
+    return;
+  }
+
+  if (method !== "GET") return res.status(400).json({ error: "Bad request!" });
+
+  const { prompt } = query;
+
+  if (!prompt) return res.status(400).json({ error: "Put a valid prompt" });
+
+  const aiClient = new ChatGPTAiClient();
+  const response = await aiClient.chat(prompt as string);
+  res.status(200).json({ response: decodeURI(response) });
+  return;
+}
