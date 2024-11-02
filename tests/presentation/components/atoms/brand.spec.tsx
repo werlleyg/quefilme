364c3ce8a50522470c62851fa8bebf218a34abbf
Werlley Ponte <werlleyponte@gmail.com>
2024-11-02 11:05:32 -0300

    test: remove unnecessary code

--- tests/presentation/components/atoms/brand.spec.tsx
+++ tests/presentation/components/atoms/brand.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Brand } from "@/presentation";
 import React from "react";
