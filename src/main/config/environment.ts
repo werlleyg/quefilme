3fe5b8b24412e6afa48cdb6ddd2a74a109c640fd
Werlley Ponte <werlleyponte@gmail.com>
2024-11-03 01:49:36 -0300

    chore: add debounceTime environment

--- src/main/config/environment.ts
+++ src/main/config/environment.ts
@@ -3,4 +3,5 @@ export const Environment = {
   accessKey: process.env.NEXT_PUBLIC_ACCESS_KEY ?? "15778c95",
   baseUrlAi:
     process.env.NEXT_PUBLIC_BASE_URL_AI ?? "http://127.0.0.1:3000/api/chat",
+  debounceTime: process.env.DEBOUNCE_TIME ?? 1,
 };
