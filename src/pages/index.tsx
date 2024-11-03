524974ada08ad562f5944e80ff9c9f1ff8634cf0
Werlley Ponte <werlleyponte@gmail.com>
2024-11-03 02:15:30 -0300

    feat(pages): create main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,92 @@
 import {
   Badge,
+  Brand,
   Button,
   CardMoviesGrid,
   Chip,
+  FullScreenModal,
+  H3,
+  Loader,
+  LoadingContent,
   MovieCard,
+  P,
+  SearchContent,
   SearchInput,
+  ShowMovie,
 } from "@/presentation";
 import Head from "next/head";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Puzzle from "../../public/assets/icons/puzzle-white.svg";
-import { MovieEntity } from "@/domain/entities";
+import { ListMoviesEntity, MovieEntity } from "@/domain/entities";
+import {
+  DivBottomContent,
+  DivChipField,
+  DivCopy,
+  DivSelectedField,
+  DivTopContent,
+  Main,
+} from "./styles";
+import { makeGetMoviesUsecase } from "@/main/factories/usecases/getMovies.factory";
+import { Environment } from "@/main/config";
+import { makeGetMovieSuggestionUsecase } from "@/main/factories/usecases/getMovieSuggestion.factory";
 
 export default function Home() {
   const [value, setValue] = useState<string>();
+  const [searchValue, setSearchValue] = useState<string>();
+  const [selectedMovies, setSelectedMovies] = useState<MovieEntity[]>([]);
+  const [searchMovies, setSearchMovies] = useState<ListMoviesEntity>();
+  const [suggestedMovie, setSuggestedMovie] = useState<MovieEntity>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleChangeInput = useCallback((value?: string) => {
     setValue(value);
   }, []);
+
+  const handleOnSelectMovie = useCallback((movie: MovieEntity) => {
+    setSelectedMovies((prev) => [...prev, movie]);
+    setValue("");
+    setSearchValue("");
+  }, []);
+
+  const handleWithRemoveMovie = useCallback((movie: MovieEntity) => {
+    setSelectedMovies((prev) => {
+      return prev.filter((prevMovie) => prevMovie !== movie);
+    });
+  }, []);
+
+  const handleWithSearchMovie = useCallback(async (searchData?: string) => {
+    if (!searchData) return setLoading(false);
+
+    const result = await makeGetMoviesUsecase().exec(searchData);
+    setSearchMovies(result);
+    setLoading(false);
+  }, []);
+
+  const handleGetSuggestMovie = useCallback(async () => {
+    setLoading(true);
+    const listMovies = new ListMoviesEntity({ movies: selectedMovies });
+    const result = await makeGetMovieSuggestionUsecase()
+      .exec(listMovies)
+      .finally(() => setLoading(false));
+    setSuggestedMovie(new MovieEntity(result));
+  }, [selectedMovies]);
+
+  const handleWithGoBack = useCallback(() => {
+    setSuggestedMovie(undefined);
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    handleWithSearchMovie(searchValue);
+  }, [handleWithSearchMovie, searchValue]);
+
+  useEffect(() => {
+    const timer = setTimeout(
+      () => setSearchValue(value),
+      Number(Environment.debounceTime) * 1000,
+    );
+    return () => clearTimeout(timer);
+  }, [value]);
 
   return (
     <>
@@ -26,14 +96,70 @@ export default function Home() {
           name="description"
           content="Descubra filmes e séries incríveis baseados nas suas escolhas anteriores. O Que Filme sugere os melhores filmes para sua próxima sessão de cinema."
         />
-
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1,  user-scalable=no"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <Main>
+        <DivTopContent>
+          <Brand style={{ marginInline: "auto" }} />
+          <DivCopy>
+            <P style={{ textAlign: "center", fontWeight: "500" }}>
+              Escolha seus filmes favoritos e descubra o próximo que vai te
+              conquistar. Nosso algoritmo faz o trabalho duro para você.{" "}
+            </P>
+            <P style={{ textAlign: "center", fontWeight: "500" }}>
+              Relaxe e aproveite a maratona!
+            </P>
+          </DivCopy>
+          <SearchContent
+            handleChangeInput={handleChangeInput}
+            value={searchValue}
+            onSelect={handleOnSelectMovie}
+            movies={searchMovies ?? undefined}
+          />
+
+          <DivSelectedField>
+            <H3>Selecionados</H3>
+
+            <DivChipField>
+              {selectedMovies.length >= 1 ? (
+                selectedMovies?.map((movie, idx) => (
+                  <Chip key={idx} onClick={() => handleWithRemoveMovie(movie)}>
+                    {movie.title}
+                  </Chip>
+                ))
+              ) : (
+                <P style={{ fontWeight: "300", opacity: "0.7" }}>
+                  Nenhum filme selecionado ainda
+                </P>
+              )}
+            </DivChipField>
+          </DivSelectedField>
+
+          <Button
+            icon={<Puzzle />}
+            onClick={handleGetSuggestMovie}
+            fullWidth
+            disabled={selectedMovies.length === 0}
+          >
+            Sugerir filme
+          </Button>
+        </DivTopContent>
+        <DivBottomContent>
+          <P style={{ fontWeight: 500, textAlign: "center" }}>
+            Desenvolvido por <u>Werlley Ponte</u>
+          </P>
+        </DivBottomContent>
+        <ShowMovie
+          movie={suggestedMovie}
+          onChangeAnotherMovie={handleGetSuggestMovie}
+          goBack={handleWithGoBack}
+        />
+        {loading && <LoadingContent />}
+      </Main>
     </>
   );
 }
