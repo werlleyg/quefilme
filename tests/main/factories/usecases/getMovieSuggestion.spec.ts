59a1dcfcc6ff28799e3d2eac0d3c59973b968e53
Werlley Ponte <werlleyponte@gmail.com>
2025-05-25 18:41:06 -0300

    tests: update unit tests

--- tests/main/factories/usecases/getMovieSuggestion.spec.ts
+++ tests/main/factories/usecases/getMovieSuggestion.spec.ts
@@ -1,28 +1,25 @@
 import { MoviesRepositoryImpl } from "@/infrastructure/repositories";
 
 import { MoviesRepository } from "@/domain/repositories";
-import { AiService } from "@/domain/services";
-import { GetMovieSuggestionUsecase, GetMoviesUsecase } from "@/domain/usecases";
+
+import { GetMovieSuggestionUsecase } from "@/domain/usecases/interfaces";
 import { AxiosHttpClient } from "@/infrastructure/http";
 import { Environment } from "@/main/config";
-import { AiServiceImpl } from "@/infrastructure/services";
+
 import { GetMovieSuggestionUsecaseImpl } from "@/domain/usecases/getMovieSuggestion.usecase";
 
 type SutTypes = {
   axiosHttpClient: AxiosHttpClient;
-  aiService: AiService;
   moviesRepository: MoviesRepository;
   sut: GetMovieSuggestionUsecase;
 };
 
 const makeSut = (): SutTypes => {
   const axiosHttpClient = new AxiosHttpClient();
-  const aiService = new AiServiceImpl(axiosHttpClient, Environment.baseUrlAi);
   const moviesRepository = new MoviesRepositoryImpl(axiosHttpClient);
-  const sut = new GetMovieSuggestionUsecaseImpl(moviesRepository, aiService);
+  const sut = new GetMovieSuggestionUsecaseImpl(moviesRepository);
   return {
     sut,
-    aiService,
     moviesRepository,
     axiosHttpClient,
   };
